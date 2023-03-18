3c732035819c8589b7443c48b36e4a7e7e7ef9c2
grfort <51990091+grfort@users.noreply.github.com>
2023-03-19 00:15:22 +0800

    added webcamera functions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import { useState, useEffect } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import { Storage } from 'aws-amplify'
+import './App.css';
+import { Camera } from "react-camera-pro";
 
 function App() {
-  const [images, setImages] = useState([])
+  const [images, setImages] = useState([]);
+  const camera = useRef(null);
+  const [numberOfCameras, setNumberOfCameras] = useState(0);
+  const [image, setImage] = useState(null);
+  const [nutri, setNutri] = useState("a");
+
   useEffect(() => {
     fetchImages()
   }, [])
@@ -23,9 +30,42 @@ function App() {
     console.log({ result })
     fetchImages()
   }
+
+  async function uploadImageToS3(image) {
+    const fileName = `testing_${new Date().getTime()}.jpeg`;
+  
+    try {
+      const result = await Storage.put(fileName, image, {
+        contentType: 'image/jpeg'
+      });
+  
+      console.log('Uploaded file: ', result);
+  
+      return result.key;
+    } catch (error) {
+      console.error('Error uploading file: ', error);
+    }
+  }
+
   return (
     <div className="App">
-      <h1>Test</h1>
+
+      <h1>Camera</h1>
+
+      <div>
+        <Camera ref={camera} aspectRatio={16 / 9} className="camera"/>
+        <button onClick={() => {const photo = camera.current.takePhoto();
+        setImage(photo)}}>Take photo</button>
+        <img src={image} alt='Taken photo'/>
+      </div>
+
+
+      <h1>Confirm Photo</h1>
+      <button onClick={() => {uploadImageToS3(image); console.log(
+              JSON.stringify({ b64: image })
+            ) }}>Upload to S3</button>
+
+      <h1>Upload existing Photo</h1>
       <div style={{ display: 'flex', flexDirection: 'column' }}>
         {
           images.map(image => (
